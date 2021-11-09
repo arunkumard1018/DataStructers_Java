a7da289672587e5d49501713790184aa9ba8bd30
ArunKumar_D <arunkumard1018@gmail.com>
2021-11-09 21:24:42 +0530

    LinkedList Implimentation continued

--- src/DataStructers/LinkedList.java
+++ src/DataStructers/LinkedList.java
@@ -31,6 +31,19 @@ public class LinkedList {
     }
 
     /**
+     * to insert node After the given index
+     * @param data
+     */
+    public void insertAfter(int index,int data) {
+        int size = size();
+        Node start =head,NewNode = new Node(data);
+        if(index > size && head==null) return;
+        for(int i=0;i<=index-1;i++) start=start.next;
+        NewNode.next = start.next;
+        start.next = NewNode;
+    }
+
+    /**
      * To insert Node at the End of the LinkedList
      */
     public void append(int data) {
@@ -46,16 +59,89 @@ public class LinkedList {
     }
 
     /**
-     * to insert node After the given index
+     * to insert the node before the given index
+     * @param index
      * @param data
      */
-    public void insertAfter(int index,int data) {
+    public void insertBefore(int index, int data) {
         int size = size();
-        Node start =head,NewNode = new Node(data);
-        if(index > size && head==null) return;
-        for(int i=0;i<=index-1;i++) start=start.next;
-        NewNode.next = start.next;
-        start.next = NewNode;
+        Node newNode = new Node(data),start = head;
+        if(index > size || head==null) return;
+        for (int i = 0; i <=index - 2; i++) {
+            start = start.next;
+        }
+        newNode.next = start.next;
+        start.next = newNode;
+    }
+
+    /**
+     * if LinkedList is Empty to create a LinkedList of given array
+     * or to add an array elements at the end of the LinkedList if LinkedList not Empty
+     * @param arr
+     */
+    public void create(int[] arr) {
+        Node start = head,newNode;
+        int loopstart = 0;
+        if (start != null) {
+            while(start.next != null) start=start.next;
+        }else{
+            head = new Node(arr[loopstart++]);
+            start = head;
+        }
+        for (int i = loopstart; i < arr.length;i++) {
+            newNode = new Node(arr[i]);
+            start.next = newNode;
+            start = newNode;
+        }
+    }
+
+    /**
+     * @return -1 if the list is empty
+     * else return data of first node and delete first node
+     */
+    public int pop() {
+        if(isEmpty()) return -1;
+        int data = head.data;
+        head = head.next;
+        return data;
+    }
+
+    /**
+     * @return Element of last node and delete the last node
+     */
+    public int remove() {
+        Node start = head;
+        if(head == null) return -1;
+        //if The LinkedList contains only one node
+        if (head.next == null) {
+            int data = head.data;
+            head = null;
+            return data;
+        }
+        /**
+         * if the LinkedList contains more than one node
+         * to travers up to before the last node
+         */
+        while (start.next.next != null) {
+            start = start.next;
+        }
+        int data = start.next.data;
+        start.next = null;
+        return data;
+    }
+
+    /**
+     * @param index to be removed
+     */
+    public void remove(int index) {
+        if(isEmpty() || index >= size()) return;
+        Node start = head;
+        if (index == 0) {
+            head = head.next;
+            return;
+        }
+        for(int i=0;i<index-1;i++) start = start.next;
+        start.next = start.next.next;
     }
 
     /**
@@ -72,9 +158,16 @@ public class LinkedList {
     }
 
     /**
+     * @return True if the LinkedList is Empty
+     */
+    public boolean isEmpty() {
+        return head==null;
+    }
+
+    /**
      * To Display the all Elements of LinkedList
      */
-    public void Display() {
+    public void display() {
         Node start = head;
         while (start != null) {
             System.out.print(start.data+" ");
--- src/DataStructers/main.java
+++ src/DataStructers/main.java
@@ -4,12 +4,12 @@ import DataStructers.LinkedList.*;
 public class main {
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
-        ll.push(12);ll.push(45);ll.push(25);
-        ll.append(100);
-        ll.Display();
-        System.out.println(ll.size());
-        ll.insertAfter(3,500);
-        System.out.println(ll.size());
-        ll.Display();
+//        ll.push(22);//ll.push(200);
+        ll.create(new int[]{2,4,6,8,10,12});
+        ll.display();
+        System.out.println();
+        ll.remove(5);
+//        System.out.println(ll.remove());
+        ll.display();
     }
 }
